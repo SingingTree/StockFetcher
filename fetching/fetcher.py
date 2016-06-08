169d730b21cbab245518e701259c2ab97a76217c
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-06-08 22:36:49 +1200

    Rework getting started code into function

--- fetching/fetcher.py
+++ fetching/fetcher.py
@@ -1,10 +1,12 @@
 import requests
 
-URL_ROOT = "http://query.yahooapis.com/v1/public/yql"
+
 
 stock_code = 'FPH.NZ'
 
-params = {'q': 'select * from yahoo.finance.quotes where symbol = "%s"' % stock_code, 'format': 'json', 'env': 'http://datatables.org/alltables.env' }
-
-r = requests.get(URL_ROOT, params)
-print(r.json())
+def fetch_quote_for_stock(stock_code):
+    URL_ROOT = "http://query.yahooapis.com/v1/public/yql"
+    params = {'q': 'select * from yahoo.finance.quotes where symbol = "%s"' % stock_code, 'format': 'json',
+              'env': 'http://datatables.org/alltables.env'}
+    r = requests.get(URL_ROOT, params)
+    return r.json()
