d79b7613da9524479078b0bd8fd57b86a3c59dbf
Bryce Van Dyk <bryce@vandyk.net.nz>
2016-06-23 19:48:58 -0400

    Little update to stock info to allow for more testing

--- persistence/persitence.py
+++ persistence/persitence.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Date, ForeignKey, create_engine
+from sqlalchemy import Column, String, Date, Float, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -13,11 +13,12 @@ class StockInfo(Base):
     __tablename__ = 'sotck_info'
     symbol = Column(String(10), ForeignKey('stocks.symbol'), primary_key=True)
     date = Column(Date, primary_key=True)
+    price_earnings_ratio = Column(Float)
 
 class Persistor:
     def __init__(self):
         self.engine = create_engine('sqlite:///stocks.db', echo=True)
         self.session = sessionmaker(bind=self.engine)
 
-    def session(self):
+    def get_session(self):
         return self.session()
