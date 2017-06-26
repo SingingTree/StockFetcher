4ab02bbd353aa519784abf6dec8377097a13167b
Bryce Van Dyk <bryce@vandyk.net.nz>
2017-06-25 22:55:40 -0400

    Update request usage in symbol fetching

--- fetching/fetcher.py
+++ fetching/fetcher.py
@@ -1,12 +1,9 @@
 import requests
-
-
-
-stock_code = 'FPH.NZ'
+import yql
 
 def fetch_quote_for_stock(stock_code):
-    URL_ROOT = "http://query.yahooapis.com/v1/public/yql"
-    params = {'q': 'select * from yahoo.finance.quotes where symbol = "%s"' % stock_code, 'format': 'json',
+    params = {'q': 'select * from yahoo.finance.quotes where symbol = "%s"' % stock_code,
+              'format': 'json',
               'env': 'http://datatables.org/alltables.env'}
-    r = requests.get(URL_ROOT, params)
+    r = requests.get(yql.get_url_root(), params)
     return r.json()
